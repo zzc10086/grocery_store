e9b106ad81d54542058a0b0a5feb8cda1a1ebaca
健忘症 <50003754+zzc10086@users.noreply.github.com>
2022-10-12 05:42:15 +0800

    修复搜索时返回请求被拦截的问题
    搜索接口强制需要buvid3这个cookie,应该与账号无关,访问`https://api.bilibili.com/x/frontend/finger/spi`可直接获取

--- bili_proxy/BPplayurl.php
+++ bili_proxy/BPplayurl.php
@@ -1,4 +1,4 @@
-<?php
+ <  ? php
 
 /* Config */
 
@@ -16,68 +16,76 @@ $req_referer = $_SERVER['HTTP_REFERER'];
 $request_headers = getallheaders();
 $request_body = file_get_contents('php://input');
 
+//通过request_query字符串生成queries键值对
+foreach(explode('&', $request_query)as $value) {
+    $request_query_tmp = explode('=', $value);
+    $queries[$request_query_tmp[0]] = $request_query_tmp[1];
+}
 
 /*tool*/
 //某个字符串在另一个字符串第N此出现的下标
-function str_n_pos($str,$find,$n)
-{
-    $pos_val=0;
-        for ($i=1;$i<=$n;$i++){
-            $pos = strpos($str,$find);
-            $str = substr($str,$pos+1);
-            $pos_val=$pos+$pos_val+1;
-        }
-	$count = $pos_val-1;
-	return $count; 
+function str_n_pos($str, $find, $n) {
+    $pos_val = 0;
+    for ($i = 1; $i <= $n; $i++) {
+        $pos = strpos($str, $find);
+        $str = substr($str, $pos + 1);
+        $pos_val = $pos + $pos_val + 1;
+    }
+    $count = $pos_val - 1;
+    return $count;
 }
 
-function array_remove_by_key($arr, $key)
-{
-	if(!array_key_exists($key, $arr)){
-		return $arr;
-	}
-	$keys = array_keys($arr);
-	$index = array_search($key, $keys);
-	if($index !== FALSE){
-		array_splice($arr, $index, 1);
-	}
+function array_remove_by_key($arr, $key) {
+    if (!array_key_exists($key, $arr)) {
+        return $arr;
+    }
+    $keys = array_keys($arr);
+    $index = array_search($key, $keys);
+    if ($index !== FALSE) {
+        array_splice($arr, $index, 1);
+    }
 
-	return $arr;
+    return $arr;
 }
 
 /* Forward request */
 $ch = curl_init();
 
 //处理请求相关header
-$request_headers = array_remove_by_key($request_headers,'Host');
-$request_headers = array_remove_by_key($request_headers,'X-Forwarded-For');
+$request_headers = array_remove_by_key($request_headers, 'Host');
+$request_headers = array_remove_by_key($request_headers, 'X-Forwarded-For');
 //配置body压缩方式
-$request_headers = array_remove_by_key($request_headers,'Accept-Encoding');
-curl_setopt($ch, CURLOPT_ENCODING, "identity");//好像b站只有br压缩
+$request_headers = array_remove_by_key($request_headers, 'Accept-Encoding');
+curl_setopt($ch, CURLOPT_ENCODING, "identity"); //好像b站只有br压缩
 
 $headers = array();
-foreach ($request_headers as $key => $value) {
-	$headers[] = $key . ': ' . $value;
+foreach($request_headers as $key => $value) {
+    $headers[] = $key.': '.$value;
 }
 
 //判断请求接口
-if(substr_count($request_uri,'/search/type')!=0){
-	$url = $upstream_pc_search_url . '?' .$request_query;
-	curl_setopt($ch, CURLOPT_REFERER, $req_referer);
-}elseif (substr_count($request_uri,'playurl')!=0){
-	//判断使用的那个pc还是app接口
-	if(substr_count($request_query,'platform=android')!=0){
-		$url = $upstream_app_url . '?' .$request_query;
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Bilibili Freedoooooom/MarkII');
-	}else{
-		$url = $upstream_pc_url . '?' .$request_query;
-		curl_setopt($ch, CURLOPT_REFERER, $req_referer);
-	}
-}else{
-	header('HTTP/1.1 502 Bad Gateway');
-	header('Content-Type: text/plain');
-	echo 'Failed to match interface./r/n';
-	return 1;
+if (substr_count($request_uri, '/search/type') != 0) {
+    //搜索接口强制需要buvid3这个cookie,应该与账号无关,访问https://api.bilibili.com/x/frontend/finger/spi可直接获取
+    if (substr_count($request_query, 'buvid3') != 0)
+        array_push($headers, "cookie: buvid3=".$queries['buvid3']);
+    $url = $upstream_pc_search_url.'?'.$request_query;
+    curl_setopt($ch, CURLOPT_REFERER, $req_referer);
+}
+elseif(substr_count($request_uri, 'playurl') != 0) {
+    //判断使用的那个pc还是app接口
+    if (substr_count($request_query, 'platform=android') != 0) {
+        $url = $upstream_app_url.'?'.$request_query;
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Bilibili Freedoooooom/MarkII');
+    } else {
+        $url = $upstream_pc_url.'?'.$request_query;
+        curl_setopt($ch, CURLOPT_REFERER, $req_referer);
+    }
+}
+else {
+    header('HTTP/1.1 502 Bad Gateway');
+    header('Content-Type: text/plain');
+    echo 'Failed to match interface./r/n';
+    return 1;
 }
 
 //url配置
@@ -93,41 +101,40 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 
 if ($response === false) {
-	header('HTTP/1.1 502 Bad Gateway');
-	header('Content-Type: text/plain');
-	echo 'Upstream host did not respond.';
+    header('HTTP/1.1 502 Bad Gateway');
+    header('Content-Type: text/plain');
+    echo 'Upstream host did not respond.';
 } else {
-	$header_length = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-	$response_headers = explode("\n", substr($response, 0, $header_length));
-	$response_body = substr($response, $header_length);
-	//处理返回相关header
-	foreach ($response_headers as $n => $response_header) {
-		//配置返回的body压缩方式
+    $header_length = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $response_headers = explode("\n", substr($response, 0, $header_length));
+    $response_body = substr($response, $header_length);
+    //处理返回相关header
+    foreach($response_headers as $n => $response_header) {
+        //配置返回的body压缩方式
         if (strpos($response_header, "Content-Encoding") !== false) {
             $response_headers[$n] = "Content-Encoding: identity\n";
         }
-		//删除B站返回的Content-Length,防止浏览器只读取Content-Length长度的数据,造成json不完整
-		if (strpos($response_header, "Content-Length") !== false) {
+        //删除B站返回的Content-Length,防止浏览器只读取Content-Length长度的数据,造成json不完整
+        if (strpos($response_header, "Content-Length") !== false) {
             unset($response_headers[$n]);
-			header('Content-Length: '.strlen($response_body));
+            //header('Content-Length: '.strlen($response_body));
         }
     }
-	unset($response_header); 
-	
-	
-	/*跨域问题*/
-	header('access-control-allow-credentials: true');
-	header('access-control-allow-origin: '. substr( $req_referer,0,str_n_pos($req_referer,'/',3)));
-	header('access-control-allow-headers: *');
-	
-	foreach ($response_headers as $header) {
-		$header = trim($header);
-		if ($header) {
-			header(trim($header));
-		}
-	}
-	echo $response_body;
+    unset($response_header);
+
+    /*跨域问题*/
+    header('access-control-allow-credentials: true');
+    header('access-control-allow-origin: '.substr($req_referer, 0, str_n_pos($req_referer, '/', 3)));
+    header('access-control-allow-headers: *');
+
+    foreach($response_headers as $header) {
+        $header = trim($header);
+        if ($header) {
+            header(trim($header));
+        }
+    }
+    echo $response_body;
 }
 
 curl_close($ch);
-?>
+?  >
