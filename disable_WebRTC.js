147891fd0f4991eb6b91bcd327785ef51b1196d1
健忘症 <50003754+zzc10086@users.noreply.github.com>
2025-12-20 21:50:49 +0800

    优化遍历

--- disable_WebRTC.js
+++ disable_WebRTC.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         禁用WebRTC
 // @description  阻止直播平台使用p2p(PCDN)技术占用上传
-// @version      0.1
+// @version      0.2
 // @author       zzc10086
 // @license      MIT
 // @namespace    https://github.com/zzc10086/grocery_store
@@ -12,37 +12,51 @@
 // @grant        none
 // ==/UserScript==
 
+const webrtcKeys = new Set([
+    "RTCCertificate", "RTCDataChannel", "RTCIceCandidate", "RTCIceTransport",
+    "RTCPeerConnection", "RTCRtpReceiver", "RTCSessionDescription",
+    "mozRTCIceCandidate", "mozRTCPeerConnection", "mozRTCSessionDescription",
+    "webkitRTCPeerConnection", "RTCIceServer", "RTCStatsReport"
+]);
 
-const webrtcThings = [
-    "RTCCertificate",
-    "RTCDataChannel",
-    "RTCIceCandidate",
-    "RTCIceTransport",
-    "RTCPeerConnection",
-    "RTCRtpReceiver",
-    "RTCSessionDescription",
-    "mozRTCIceCandidate",
-    "mozRTCPeerConnection",
-    "mozRTCSessionDescription",
-    "webkitRTCPeerConnection",
-];
-
-for (const t of webrtcThings) delete window[t];
+// 1. 禁用当前窗口的 WebRTC 属性
+webrtcKeys.forEach(key => {
+    if (window[key]) {
+        try {
+            Object.defineProperty(window, key, {
+                value: undefined,
+                writable: false,
+                configurable: false
+            });
+        } catch (e) {
+            delete window[key];
+        }
+    }
+});
 
 //b站会检测iframe的window对象来判断当前页面是否被hack
 const originalContentWindow = Object.getOwnPropertyDescriptor(HTMLIFrameElement.prototype, 'contentWindow').get;
 Object.defineProperty(HTMLIFrameElement.prototype, 'contentWindow', {
+    configurable: false,
+    enumerable: true,
     get: function() {
-        const rawWin = originalContentWindow.call(this);
-        return new Proxy(rawWin, {
+        const win = originalContentWindow.call(this);
+        if (!win) return win;
+
+        return new Proxy(win, {
             get(target, prop) {
-                for (const t of webrtcThings) {
-                    if (t == prop) return null;
+                if (webrtcKeys.has(prop)) {
+                    return undefined;
                 }
-                return typeof target[prop] === 'function' ? target[prop].bind(target) : target[prop];
+
+                const value = Reflect.get(target, prop);
+                if (typeof value === 'function') {
+                    return value.bind(target);
+                }
+                return value;
             }
         });
     }
 });
 
-console.log("Disable P2P script run");
+console.log("已拦截 WebRTC 相关接口");
